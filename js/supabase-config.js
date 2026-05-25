6abdbc21da16c8e4076c99381301e5a409a80744
Morikutoku <dizzykin0@gmail.com>
2026-05-25 20:20:31 +0000

    Update Paystack Public Key

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,5 +4,5 @@
 window.NEXUZ_SUPABASE_CONFIG = {
   url: 'https://onanhpeqttqdruruemwr.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uYW5ocGVxdHRxZHJ1cnVlbXdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5MzQ2NDMsImV4cCI6MjA5NDUxMDY0M30.aU63Ch6OzLHfGhWc08Fcbmmx0dl-GtogGKiHdYlFOc8',
-  paystackPublicKey: 'pk_test_xxxxxxxxxxxxxxxxxxxxxxxx' // Replace with your real public key
+  paystackPublicKey: 'pk_live_6cbe56805bd5222977f289a01e435794e50415dd'
 };
